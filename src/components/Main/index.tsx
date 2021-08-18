339f2f7c8126abd7c49c00ed5f2ad2a639a6b3d1
Augusto Barreira <barreira266@hotmail.com>
2021-08-18 07:37:19 -0300

    config storybook and pwa

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,21 @@
-const Main = () => (
-  <div>
-    <h1>React avançado</h1>
-  </div>
+import * as S from './styles'
+
+const Main = ({
+  title = 'React avançado',
+  description = 'Typescript, React js, Nexjs e Styled Components'
+}) => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado"
+    />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código"
+    />
+  </S.Wrapper>
 )
 
 export default Main
